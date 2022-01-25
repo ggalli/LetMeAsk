bac0aeb4587a1d39ed4769d75e1d4aa876c0aa67
Guilherme <guilherme@nfe.io>
2022-01-25 16:09:11 -0300

    create admin room route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/rooms/new" element={<NewRoom />} />
         <Route path="/rooms/:roomId" element={<Room />} />
+        <Route path="admin/rooms/:roomId" element={<AdminRoom />} />
       </Routes>
     </AuthContextProvider>
   );
